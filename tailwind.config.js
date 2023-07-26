ce9c7bd8645d83abdf5344a606bf30d3a79abe68
Abu sayed <saikat25112000@gmail.com>
2023-07-26 16:17:49 +0600

    another commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  
     content: ["./src/**/*.{html,js}"],
-    theme: {
+    
       extend: {
-      
+        display: ['group-focus'],
+        opacity: ['group-focus'],
+        inset: ['group-focus'],
       },
-    },
+  
     plugins: [],
   }
